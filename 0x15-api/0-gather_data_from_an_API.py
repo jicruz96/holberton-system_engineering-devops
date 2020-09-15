305844a9a778e49e6dbab4e0ddba260f0441997c
jicruz96 <accounts@jicruz.com>
2020-09-15 01:31:37 +0000

    add try/except to file 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,30 +12,34 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    # Set main variables for script
-    employee_id = argv[1]
-    url = 'https://jsonplaceholder.typicode.com/'
+    try:
+        # Set main variables for script
+        employee_id = argv[1]
+        url = 'https://jsonplaceholder.typicode.com/'
 
-    # Get employee_name
-    employee_request = url + 'users/' + employee_id
-    employee_name = requests.get(employee_request).json().get('name')
+        # Get employee_name
+        employee_request = url + 'users/' + employee_id
+        employee_name = requests.get(employee_request).json().get('name')
 
-    # Get employee task list
-    to_do_request = url + 'todos'
-    tasks = requests.get(to_do_request).json()
-    tasks = [task for task in tasks if task.get('userId') == int(employee_id)]
+        # Get employee task list
+        to_do_request = url + 'todos'
+        tasks = requests.get(to_do_request).json()
+        tasks = [task for task in tasks if task.get(
+            'userId') == int(employee_id)]
 
-    # Get number of tasks (total_tasks)
-    total_tasks = len(tasks)
+        # Get number of tasks (total_tasks)
+        total_tasks = len(tasks)
 
-    # Narrow down to completed tasks only
-    tasks = [task for task in tasks if task.get('completed') is True]
+        # Narrow down to completed tasks only
+        tasks = [task for task in tasks if task.get('completed') is True]
 
-    # Get number of completed tasks (done_tasks)
-    done_tasks = len(tasks)
+        # Get number of completed tasks (done_tasks)
+        done_tasks = len(tasks)
 
-    first_line = "Employee {} is done with tasks({}/{})"
-    first_line = first_line.format(employee_name, done_tasks, total_tasks)
-    print(first_line)
-    for task in tasks:
-        print("\t {}".format(task.get('title')))
+        first_line = "Employee {} is done with tasks({}/{})"
+        first_line = first_line.format(employee_name, done_tasks, total_tasks)
+        print(first_line)
+        for task in tasks:
+            print("\t {}".format(task.get('title')))
+    except:
+        pass
