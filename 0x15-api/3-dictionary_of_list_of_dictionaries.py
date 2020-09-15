14035bc3f80712d2c1828ef73d5861d65a7bf37a
jicruz96 <accounts@jicruz.com>
2020-09-15 06:31:55 +0000

    push new version of file 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,49 +1,34 @@
 #!/usr/bin/python3
-"""
-exports fake to-do information for all fake employees from jsonplaceholder API,
+""" exports fake to-do info for all fake employees from jsonplaceholder API """
 
-USAGE:
-    ./3-dictionary_of_list_of_dictionaries.py
-
-RESULT:
-    JSON file named todo_all_employes.json with all employee tasks
-"""
 from collections import OrderedDict
-import json
-import requests
+from json import dump
+from requests import get
 
 if __name__ == "__main__":
 
-    # Set main variables for script
-    url = 'https://jsonplaceholder.typicode.com/{}/'
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    # Get user list
-    users = requests.get(url.format('users')).json()
-    users = {str(user.get('id')): user.get('username') for user in users}
-
-    # Get task list
-    tasks = requests.get(url.format('todos')).json()
+    usernames = {}
+    for user in get(url + 'users').json():
+        user_id = str(user.get('id'))
+        usernames[user_id] = user.get('username')
 
     tasks_by_id = OrderedDict()
-    for task in tasks:
+    for task in get(url + 'todos').json():
 
-        # Create a new task dictionary for each task
-        # First, get the user_id and username
-        task_dict = OrderedDict()
         user_id = str(task.get("userId"))
-        username = users.get(user_id)
 
-        # Then, add the three task dictionary key/values
-        task_dict.update({"username": username})
-        task_dict.update({"task": task.get("title")})
-        task_dict.update({"completed": task.get("completed")})
+        task_dict = OrderedDict(
+            username=usernames.get(user_id),
+            task=task.get("title"),
+            completed=task.get("completed")
+        )
 
-        # Add this new task dictionary to the list of tasks
         if tasks_by_id.get(user_id) is None:
             tasks_by_id[user_id] = [task_dict]
         else:
             tasks_by_id[user_id].append(task_dict)
 
-    # Write JSON file
     with open('todo_all_employees.json', 'w') as json_file:
-        json.dump(tasks_by_id, json_file)
+        dump(tasks_by_id, json_file)
