98de410ec45496f7c237ee40149d6bb7f5f3d2a4
jicruz96 <accounts@jicruz.com>
2020-09-16 16:05:46 +0000

    refactor file 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,26 +9,23 @@ if __name__ == "__main__":
 
     url = 'https://jsonplaceholder.typicode.com/'
 
-    usernames = {}
+    unames_by_id = {}
     for user in get(url + 'users').json():
         user_id = str(user.get('id'))
-        usernames[user_id] = user.get('username')
+        unames_by_id[user_id] = user.get('username')
 
-    tasks_by_id = OrderedDict()
+    tasks_by_id = {user_id: [] for user_id in unames_by_id}
     for task in get(url + 'todos').json():
 
         user_id = str(task.get("userId"))
 
-        task_dict = OrderedDict(
-            username=usernames.get(user_id),
-            task=task.get("title"),
-            completed=task.get("completed")
-        )
+        task_dict = {
+            "username": unames_by_id.get(user_id),
+            "task": task.get("title"),
+            "completed": task.get("completed")
+        }
 
-        if tasks_by_id.get(user_id) is None:
-            tasks_by_id[user_id] = [task_dict]
-        else:
-            tasks_by_id[user_id].append(task_dict)
+        tasks_by_id[user_id].append(task_dict)
 
     with open('todo_all_employees.json', 'w') as json_file:
         dump(tasks_by_id, json_file)
