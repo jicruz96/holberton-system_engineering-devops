0bbbe468e61a99109af9a10f69b2c1f577bdfd9f
jicruz96 <accounts@jicruz.com>
2020-09-17 21:34:03 +0000

    refactor file 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,19 +2,17 @@
 """ defines the top_ten function """
 
 from requests import get
-from urllib.parse import urlencode
 
 
 def top_ten(subreddit):
     """ Returns the titles of the top 10 hot posts for a given subreddit """
 
     settings = {'allow_redirects': False, 'headers': {'User-agent': ''}}
-    url = "https://www.reddit.com/r/{}/hot.json"
-    url = url.format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
     try:
         responses = get(url, **settings).json().get('data').get('children')
-        posts = [post['data']['title'] for post in responses][:10]
-        for post in posts:
-            print(post)
+        for post in responses[:10]:
+            print(post['data']['title'])
     except:
         print("None")
