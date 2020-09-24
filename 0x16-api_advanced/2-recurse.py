32443fe947a9f7485d1fd00c8def7f9b07f4d642
jicruz96 <accounts@jicruz.com>
2020-09-24 00:18:12 +0000

    Change approach for file 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,20 +2,26 @@
 """ defines the recurse function """
 
 from requests import get
+from json import dump
+from urllib.parse import urlencode
 
 
-def recurse(subreddit, last_post="", hot_list=[]):
+def recurse(subreddit, **kwargs):
     """ returns list of all hot article titles for a given subreddit.
         If no results are found, returns None.
     """
-    try:
-        if last_post is None:
-            return hot_list
-        query = "?limit=100&after=" + last_post
-        url = "https://www.reddit.com/r/{}/hot.json{}".format(subreddit, query)
-        settings = {'headers': {'User-agent': ''}, 'allow_redirects': False}
-        data = get(url, **settings).json().get('data')
-        hot_list += [post['data']['title'] for post in data.get('children')]
-        return recurse(subreddit, data['after'], hot_list)
-    except:
-        return None
+    settings = {'allow_redirects': False, 'headers': {'User-agent': ''}}
+    query = "?" + urlencode(kwargs)
+    url = 'https://www.reddit.com/r/{}/hot.json{}'.format(subreddit, query)
+    response = get(url, **settings).json().get('data')
+    articles = response.get('children')
+    after = response.get('after')
+    print(after)
+    titles = []
+    for article in articles:
+        titles.append(article['data']['title'])
+    query_options = {'limit': 100, 'after': after}
+    if after is not None:
+        more_titles = recurse(subreddit, **query_options)
+        titles += more_titles
+    return titles
