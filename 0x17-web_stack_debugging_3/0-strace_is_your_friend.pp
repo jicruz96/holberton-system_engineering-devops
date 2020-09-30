a0e8ba3958022caadfde1cdbd91de918eebd818f
jicruz96 <accounts@jicruz96.com>
2020-09-30 15:59:53 +0000

    fix style issues in file 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,7 @@
 # Task 0
 # Fix Wordpress site with faulty settings
 
-
 exec { 'change phpps':
-provider => 'shell';
-command => 'cat /var/www/html/wp-settings.php | sed \'s$phpp$php$g\' | sudo tee /var/www/html/wp-settings.php',
+  provider => 'shell',
+  command  => 'cat /var/www/html/wp-settings.php | sed \'s$phpp$php$g\' | sudo tee /var/www/html/wp-settings.php',
 }
