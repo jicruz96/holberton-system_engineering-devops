39324bbc2264bfdf4cdb81aec1c209984864bc38
jicruz96 <accounts@jicruz96.com>
2020-09-30 03:45:57 +0000

    add apache restart to puppet file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,19 +1,24 @@
 # Task 0
 # Fix Wordpress site with faulty settings
-include stdlib
 
 file { '.htaccess':
-ensure => present,
-path => '/var/www/html',
+ensure => 'present',
+path => '/var/www/html/.htaccess',
 mode => '0666',
 group => 'www-data',
 owner => 'www-data',
 content => 'DirectoryIndex index.php'
 }
 
-file_line { 'wp-settings.php':
-ensure => present,
-path => '/var/www/html',
-line => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
-match => "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );"
+file_line { 'replace phpp':
+ensure => 'present',
+path => '/var/www/html/wp-settings.php',
+line => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
+match => 'class-wp-locale.phpp'
+}
+
+
+exec { 'restart apache':
+path => '/usr/bin/',
+command => 'sudo service apache2 restart'
 }
