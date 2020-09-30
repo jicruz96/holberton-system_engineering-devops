aab1add6e7a3a29ff7ecad8a058d0b537a9debdf
jicruz96 <accounts@jicruz96.com>
2020-09-30 15:55:01 +0000

    change approach to puppet because holberton won't let you use the stdlib puppet module

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,24 +1,8 @@
 # Task 0
 # Fix Wordpress site with faulty settings
 
-file { '.htaccess':
-ensure => 'present',
-path => '/var/www/html/.htaccess',
-mode => '0666',
-group => 'www-data',
-owner => 'www-data',
-content => 'DirectoryIndex index.php'
-}
 
-file_line { 'replace phpp':
-ensure => 'present',
-path => '/var/www/html/wp-settings.php',
-line => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
-match => 'class-wp-locale.phpp'
-}
-
-
-exec { 'restart apache':
-path => '/usr/bin/',
-command => 'sudo service apache2 restart'
+exec { 'change phpps':
+provider => 'shell';
+command => 'cat /var/www/html/wp-settings.php | sed \'s$phpp$php$g\' | sudo tee /var/www/html/wp-settings.php',
 }
